51fdb074661d868d8c1bc3272796f8562b158cf4
Eliot Howes <eliot@medicspot.co.uk>
2021-04-29 11:24:58 +0100

    Some new text

--- src/App.js
+++ src/App.js
@@ -91,6 +91,7 @@ const App = () => {
           </div>
         )}
       </div>
+      <h1>Some tests</h1>
       <Switch>
         <ConditionalRoute path="/consult">
           <PretendConsultation connectedUsbDevices={connectedUsbDevices} />
