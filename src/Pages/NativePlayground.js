9bed6a57a6a73b0b25254daf37db2a4285f5335e
Eliot Howes <eliot@medicspot.co.uk>
2021-11-10 16:50:35 +0000

    dont await callHandler also remove some logging

--- src/Pages/NativePlayground.js
+++ src/Pages/NativePlayground.js
@@ -4,89 +4,62 @@ import "./NativePlayground.css"
 const NativePlayground = () => {
   const startScan = async () => {
     if ('flutter_inappwebview' in window) {
-      console.log('Sending to webview: ', '\n', 'startScan')
-
-      const result = await window.flutter_inappwebview.callHandler('startScan');
-
-      console.log('\x1b[31m\x1b[47m%s\x1b[0m', '<<< Start >>>', '\n');
-      console.log(result);
-      console.log('\x1b[0m%s\x1b[32m\x1b[47m%s\x1b[0m', '\n', '<<< Finish >>>', '\n');
+      return window.flutter_inappwebview.callHandler('startScan');
     }
-    else {
-      console.log(
-        'Not running inside webview. The following was not sent: ',
-        '\n',
-        '<<<', 'startScan', '>>>'
-      )
-    }
+
+    console.log(
+      'Not running inside webview. The following was not sent: ',
+      '\n',
+      '<<<', 'startScan', '>>>'
+    )
   }
 
   const connectDevice = async (deviceIdentifier) => {
     if ('flutter_inappwebview' in window) {
-      console.log('Sending to webview: ', '\n', 'connectDevice', {deviceIdentifier})
-
-      const result = await window.flutter_inappwebview.callHandler('connectDevice', {deviceIdentifier});
-
-      console.log('Response from webview: ', '\n', result)
+      return window.flutter_inappwebview.callHandler('connectDevice', {deviceIdentifier});
     }
-    else {
-      console.log(
-        'Not running inside webview. The following was not sent: ',
-        '\n',
-        '<<<', 'connectDevice', {deviceIdentifier}, '>>>'
-      )
-    }
+
+    console.log(
+      'Not running inside webview. The following was not sent: ',
+      '\n',
+      '<<<', 'connectDevice', {deviceIdentifier}, '>>>'
+    )
   }
 
   const getManufacturerInfo = async (deviceIdentifier) => {
     if ('flutter_inappwebview' in window) {
-      console.log('Sending to webview: ', '\n', 'manufacturerInfo', {deviceIdentifier})
-
-      const result = await window.flutter_inappwebview.callHandler('manufacturerInfo', {deviceIdentifier});
-
-      console.log('Response from webview: ', '\n', result)
+      return window.flutter_inappwebview.callHandler('manufacturerInfo', {deviceIdentifier});
     }
-    else {
-      console.log(
-        'Not running inside webview. The following was not sent: ',
-        '\n',
-        '<<<', 'manufacturerInfo', {deviceIdentifier}, '>>>'
-      )
-    }
+
+    console.log(
+      'Not running inside webview. The following was not sent: ',
+      '\n',
+      '<<<', 'manufacturerInfo', {deviceIdentifier}, '>>>'
+    )
   }
 
   const getDeviceReading = async (deviceIdentifier) => {
     if ('flutter_inappwebview' in window) {
-      console.log('Sending to webview: ', '\n', 'readMeasurement', {deviceIdentifier})
-
-      const result = await window.flutter_inappwebview.callHandler('readMeasurement', {deviceIdentifier});
-
-      console.log('Response from webview: ', '\n', result)
+      return window.flutter_inappwebview.callHandler('readMeasurement', {deviceIdentifier});
     }
-    else {
-      console.log(
-        'Not running inside webview. The following was not sent: ',
-        '\n',
-        '<<<', 'readMeasurement', {deviceIdentifier}, '>>>'
-      )
-    }
+
+    console.log(
+      'Not running inside webview. The following was not sent: ',
+      '\n',
+      '<<<', 'readMeasurement', {deviceIdentifier}, '>>>'
+    )
   }
 
   const closeDevice = async (deviceIdentifier) => {
     if ('flutter_inappwebview' in window) {
-      console.log('Sending to webview: ', '\n', 'closeDevice', {deviceIdentifier})
-
-      const result = await window.flutter_inappwebview.callHandler('closeDevice', {deviceIdentifier});
-
-      console.log('Response from webview: ', '\n', result)
+      return window.flutter_inappwebview.callHandler('closeDevice', {deviceIdentifier});
     }
-    else {
-      console.log(
-        'Not running inside webview. The following was not sent: ',
-        '\n',
-        '<<<', 'closeDevice', {deviceIdentifier}, '>>>'
-      )
-    }
+
+    console.log(
+      'Not running inside webview. The following was not sent: ',
+      '\n',
+      '<<<', 'closeDevice', {deviceIdentifier}, '>>>'
+    )
   }
 
 
