b5063a264dbda76d94e61e2f9177952157e66f89
Eliot Howes <eliot@medicspot.co.uk>
2021-11-10 16:20:38 +0000

    use window.flutter_inappwebview.callHandler instead of postMessage

--- src/Pages/NativePlayground.js
+++ src/Pages/NativePlayground.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import "./NativePlayground.css"
 
 const NativePlayground = () => {
-  const startScan = () => {
-    window.postMessage('startScan')
+  const startScan = async () => {
+    if ('flutter_inappwebview' in window) {
+      const result = await window.flutter_inappwebview.callHandler('startScan');
+
+      console.log('\x1b[31m\x1b[47m%s\x1b[0m', '<<< Start >>>', '\n');
+      console.log(result);
+      console.log('\x1b[0m%s\x1b[32m\x1b[47m%s\x1b[0m', '\n', '<<< Finish >>>', '\n');
+    }
+  }
+
+  const connectDevice = async (deviceIdentifier) => {
+    if ('flutter_inappwebview' in window) {
+      const result = await window.flutter_inappwebview.callHandler('connectDevice', {deviceIdentifier});
+      console.log('\x1b[31m\x1b[47m%s\x1b[0m', '<<< Start >>>', '\n');
+      console.log(result);
+      console.log('\x1b[0m%s\x1b[32m\x1b[47m%s\x1b[0m', '\n', '<<< Finish >>>', '\n');
+    }
   }
   return (
-    <div>
+    <div className="buttons-container">
       <button onClick={startScan}>
         Start scan
+      </button>
+      <button onClick={() => connectDevice('taidoc-td1241-ble')}>
+        Connect TD-1241
       </button>
     </div>
   );
