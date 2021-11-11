fcd5a24b85f1a5b6203702c62d3d13b6335c9c47
Eliot Howes <eliot@medicspot.co.uk>
2021-11-11 16:10:30 +0000

    add event listeners for native events over rpc

--- src/Pages/NativePlayground.js
+++ src/Pages/NativePlayground.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import "./NativePlayground.css"
+import React, {useState} from 'react';
+import './NativePlayground.css'
 
 const NativePlayground = () => {
-  const startScan = async () => {
+  const [bleScanResult, setBleScanResult] = useState();
+  const [deviceConnection, setDeviceConnection] = useState();
+  const [manufacturerInfo, setManufacturerInfo] = useState();
+  const [deviceReading, setDeviceReading] = useState();
+  const [closeDeviceResult, setCloseDeviceResult] = useState();
+
+  /**
+   *
+   * BLE Scan
+   */
+  const handleScanResult = (data) => {
+    window.removeEventListener('scanResult', handleScanResult)
+
+    console.log('Received data: ', data);
+
+    setBleScanResult(data);
+  }
+
+  const startScan = async (deviceIdentifier) => {
+    // Should be able to use window.isMobileWebView = true
     if ('flutter_inappwebview' in window) {
-      return window.flutter_inappwebview.callHandler('startScan');
+      window.addEventListener('scanResult', handleScanResult)
+
+      return window.flutter_inappwebview.callHandler('startScan', {deviceIdentifier});
     }
 
     console.log(
@@ -14,8 +35,22 @@ const NativePlayground = () => {
     )
   }
 
+  /**
+   *
+   * Connect Device
+   */
+  const handleConnectDevice = (data) => {
+    window.removeEventListener('connectedDevice', handleConnectDevice)
+
+    console.log('Received data: ', data);
+
+    setDeviceConnection(data);
+  }
+
   const connectDevice = async (deviceIdentifier) => {
     if ('flutter_inappwebview' in window) {
+      window.addEventListener('connectedDevice', handleConnectDevice)
+
       return window.flutter_inappwebview.callHandler('connectDevice', {deviceIdentifier});
     }
 
@@ -26,8 +61,22 @@ const NativePlayground = () => {
     )
   }
 
+  /**
+   *
+   * Get Manufacturer Info
+   */
+  const handleManufacturerInfo = (data) => {
+    window.removeEventListener('manufacturerInfo', handleManufacturerInfo)
+
+    console.log('Received data: ', data);
+
+    setManufacturerInfo(data);
+  }
+
   const getManufacturerInfo = async (deviceIdentifier) => {
     if ('flutter_inappwebview' in window) {
+      window.addEventListener('manufacturerInfo', handleManufacturerInfo)
+
       return window.flutter_inappwebview.callHandler('manufacturerInfo', {deviceIdentifier});
     }
 
@@ -38,8 +87,22 @@ const NativePlayground = () => {
     )
   }
 
+  /**
+   *
+   * Get Device Reading
+   */
+   const handleDeviceReading = (data) => {
+    window.removeEventListener('readMeasurement', handleDeviceReading)
+
+    console.log('Received data: ', data);
+
+    setDeviceReading(data);
+  }
+
   const getDeviceReading = async (deviceIdentifier) => {
     if ('flutter_inappwebview' in window) {
+      window.addEventListener('readMeasurement', handleDeviceReading)
+
       return window.flutter_inappwebview.callHandler('readMeasurement', {deviceIdentifier});
     }
 
@@ -50,9 +113,26 @@ const NativePlayground = () => {
     )
   }
 
+  /**
+   *
+   * Close Device
+   */
+   const handleCloseDevice = (data) => {
+    window.removeEventListener('readMeasurement', handleCloseDevice)
+
+    console.log('Received data: ', data);
+
+    setCloseDeviceResult(data);
+  }
+
   const closeDevice = async (deviceIdentifier) => {
     if ('flutter_inappwebview' in window) {
-      return window.flutter_inappwebview.callHandler('closeDevice', {deviceIdentifier});
+      window.addEventListener('readMeasurement', handleCloseDevice)
+
+      return window.flutter_inappwebview.callHandler(
+        'closeDevice',
+        {device: deviceIdentifier}
+      );
     }
 
     console.log(
@@ -85,6 +165,37 @@ const NativePlayground = () => {
       <hr />
       <div>
         <h1>Output from webview</h1>
+        {bleScanResult && (
+          <div className="rpc-output">
+            <h4>BLE Scan Result</h4>
+            <pre>{JSON.stringify(bleScanResult)}</pre>
+          </div>
+        )}
+        {deviceConnection && (
+          <div className="rpc-output mt">
+            <h4>Device Connection Result</h4>
+            <pre>{JSON.stringify(deviceConnection)}</pre>
+          </div>
+        )}
+        {manufacturerInfo && (
+          <div className="rpc-output mt">
+            <h4>Manufacturer Info Result</h4>
+            <pre>{JSON.stringify(manufacturerInfo)}</pre>
+          </div>
+        )}
+        {deviceReading && (
+          <div className="rpc-output mt">
+            <h4>Device Reading</h4>
+            <pre>{JSON.stringify(deviceReading)}</pre>
+          </div>
+        )}
+        {closeDeviceResult && (
+          <div className="rpc-output mt">
+            <h4>Device Closed Result</h4>
+            <pre>{JSON.stringify(closeDeviceResult)}</pre>
+          </div>
+        )}
+        {!bleScanResult && !deviceConnection && !manufacturerInfo && !deviceReading && !closeDeviceResult }
         <pre>.....</pre>
       </div>
     </div>
