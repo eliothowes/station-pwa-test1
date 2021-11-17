c2d2bb2b3f5f40c12ccfe9f1c964d4123072d3b4
Eliot Howes <eliot@medicspot.co.uk>
2021-11-17 13:22:50 +0000

    add web side rpc

--- src/Pages/NativePlayground.js
+++ src/Pages/NativePlayground.js
@@ -1,34 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import * as ble from './utils/ble';
+import nativeRpc from './NativeRpc';
 import './NativePlayground.css'
 
 const NativePlayground = () => {
-  const [bleScanResult, setBleScanResult] = useState();
-  const [deviceConnection, setDeviceConnection] = useState();
-  const [manufacturerInfo, setManufacturerInfo] = useState();
-  const [deviceReading, setDeviceReading] = useState();
-  const [closeDeviceResult, setCloseDeviceResult] = useState();
+  const [td1241Data, setTd1241Data] = useState();
 
   const [rpcResponse, setRpcResponse] = useState();
 
   const handlePostMessage = (event) => {
     if (event.origin !== "https://dreamy-khorana-b8a419.netlify.app") {
       return;
-    }
-
-    const {messageType, value} = event.data;
-
-    setRpcResponse(event.data)
-
-    switch (messageType) {
-      case "scanResult":
-        setBleScanResult(value)
-        break;
-      case "connectedDevice":
-        setDeviceConnection(value)
-        break;
-      default:
-        setRpcResponse(event.data)
     }
   }
 
@@ -42,9 +23,13 @@ const NativePlayground = () => {
    *
    * BLE Scan
    */
-  const startScan = async () => {
+  const getDeviceAndMeasurement = async (deviceIdentifier) => {
     if (window.isMobileWebView) {
-      return ble.startScan();
+      const deviceReading = await nativeRpc.getDeviceAndMeasurement(deviceIdentifier);
+
+      console.log(deviceReading)
+      setRpcResponse(deviceReading)
+      setTd1241Data(deviceReading)
     }
 
     console.log(
@@ -56,89 +41,15 @@ const NativePlayground = () => {
 
   /**
    *
-   * Connect Device
-   */
-  const connectDevice = async (deviceIdentifier) => {
-    if (window.isMobileWebView) {
-      return ble.connectDevice(deviceIdentifier);
-    }
-
-    console.log(
-      'Not running inside webview. The following was not sent: ',
-      '\n',
-      '<<<', 'connectDevice', {deviceIdentifier}, '>>>'
-    )
-  }
-
-  /**
-   *
-   * Get Manufacturer Info
-   */
-  const handleManufacturerInfo = (data) => {
-    window.removeEventListener('manufacturerInfo', handleManufacturerInfo)
-
-    console.log('Received data: ', data);
-
-    setManufacturerInfo(data.detail);
-  }
-
-  const getManufacturerInfo = async (deviceIdentifier) => {
-    if (window.isMobileWebView) {
-      window.addEventListener('manufacturerInfo', handleManufacturerInfo)
-
-      return window.flutter_inappwebview.callHandler('manufacturerInfo', {device: deviceIdentifier});
-    }
-
-    console.log(
-      'Not running inside webview. The following was not sent: ',
-      '\n',
-      '<<<', 'manufacturerInfo', {deviceIdentifier}, '>>>'
-    )
-  }
-
-  /**
-   *
-   * Get Device Reading
-   */
-   const handleDeviceReading = (data) => {
-    window.removeEventListener('readMeasurement', handleDeviceReading)
-
-    console.log('Received data: ', data);
-
-    setDeviceReading(data.detail);
-  }
-
-  const getDeviceReading = async (deviceIdentifier) => {
-    if (window.isMobileWebView) {
-      window.addEventListener('readMeasurement', handleDeviceReading)
-
-      return window.flutter_inappwebview.callHandler('readMeasurement', {device: deviceIdentifier});
-    }
-
-    console.log(
-      'Not running inside webview. The following was not sent: ',
-      '\n',
-      '<<<', 'readMeasurement', {deviceIdentifier}, '>>>'
-    )
-  }
-
-  /**
-   *
    * Close Device
    */
-   const handleCloseDevice = (data) => {
-    window.removeEventListener('readMeasurement', handleCloseDevice)
-
-    console.log('Received data: ', data);
-
-    setCloseDeviceResult(data.detail);
-  }
-
   const closeDevice = async (deviceIdentifier) => {
     if (window.isMobileWebView) {
-      window.addEventListener('readMeasurement', handleCloseDevice)
+      const deviceReading = await nativeRpc.closeDevice(deviceIdentifier);
 
-      return window.flutter_inappwebview.callHandler('closeDevice', {device: deviceIdentifier});
+      console.log(deviceReading)
+      setRpcResponse(deviceReading)
+      setTd1241Data(deviceReading)
     }
 
     console.log(
@@ -152,17 +63,8 @@ const NativePlayground = () => {
   return (
     <div>
       <div className="buttons-container">
-        <button onClick={startScan}>
-          Start Scan
-        </button>
-        <button onClick={() => connectDevice('taidoc-td1241-ble')}>
-          Connect TD-1241
-        </button>
-        <button onClick={() => getManufacturerInfo('taidoc-td1241-ble')}>
-          Get Manufacturer Info about TD-1241
-        </button>
-        <button onClick={() => getDeviceReading('taidoc-td1241-ble')}>
-          Get Reading From TD-1241
+        <button onClick={() => getDeviceAndMeasurement('taidoc-td1241-ble')}>
+          Connect to TD-1241 and get reading
         </button>
         <button onClick={() => closeDevice('taidoc-td1241-ble')}>
           Close TD-1241
@@ -171,34 +73,10 @@ const NativePlayground = () => {
       <hr />
       <div>
         <h1>Output from webview</h1>
-        {bleScanResult && (
-          <div className="rpc-output">
-            <h4>BLE Scan Result</h4>
-            <pre>{JSON.stringify(bleScanResult, null, 2)}</pre>
-          </div>
-        )}
-        {deviceConnection && (
-          <div className="rpc-output mt">
-            <h4>Device Connection Result</h4>
-            <pre>{JSON.stringify(deviceConnection, null, 2)}</pre>
-          </div>
-        )}
-        {manufacturerInfo && (
-          <div className="rpc-output mt">
-            <h4>Manufacturer Info Result</h4>
-            <pre>{JSON.stringify(manufacturerInfo, null, 2)}</pre>
-          </div>
-        )}
-        {deviceReading && (
+        {td1241Data && (
           <div className="rpc-output mt">
             <h4>Device Reading</h4>
-            <pre>{JSON.stringify(deviceReading, null, 2)}</pre>
-          </div>
-        )}
-        {closeDeviceResult && (
-          <div className="rpc-output mt">
-            <h4>Device Closed Result</h4>
-            <pre>{JSON.stringify(closeDeviceResult, null, 2)}</pre>
+            <pre>{JSON.stringify(td1241Data, null, 2)}</pre>
           </div>
         )}
         {rpcResponse && (
@@ -207,7 +85,7 @@ const NativePlayground = () => {
             <pre>{JSON.stringify(rpcResponse, null, 2)}</pre>
           </div>
         )}
-        {(!bleScanResult || !deviceConnection || !manufacturerInfo || !deviceReading || !closeDeviceResult) && (
+        {(!td1241Data || !rpcResponse) && (
           <pre>.....</pre>
         )}
       </div>
---
+++ src/Pages/NativeRpc.js
@@ -0,0 +1,101 @@
+class NativeRpc {
+  constructor () {
+    this._messageId = 0;
+  }
+
+  _rpcSuccessful (rpcMessage, messageType, currentMessageId) {
+    const {ok, type, messageId} = rpcMessage;
+
+    return ok && type === messageType && messageId === currentMessageId
+  }
+
+  getDeviceAndMeasurement (deviceIdentifier) {
+    const messageId = this._messageId++
+
+    const message = {
+      type: 'getDeviceAndMeasurement',
+      messageId,
+      data: {
+        device: deviceIdentifier
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      // If the native app hasn't responded in 3 secs then timeout
+      const rpcTimeout = setTimeout(() => {
+        window.removeEventListener('message', returnHandler);
+        reject(new Error(`Timeout calling function: ${message.type}`));
+      }, 3 * 1000);
+
+      const returnHandler = (event) => {
+        const {data: message} = event.data;
+
+        if (this._rpcSuccessful(message, messageId, 'deviceAndMeasurementResult')) {
+          clearTimeout(rpcTimeout);
+          window.removeEventListener('message', returnHandler);
+          resolve(message.response);
+        } else {
+          clearTimeout(rpcTimeout);
+          window.removeEventListener('message', returnHandler);
+          reject(new Error(message.error));
+        }
+      };
+
+      window.addEventListener('message', returnHandler);
+
+      if ('flutter_inappwebview' in window) {
+        return window.flutter_inappwebview.callHandler('getDeviceAndMeasurement', message);
+      }
+
+      if ('ReactNativeWebView' in window) {
+        return window.ReactNativeWebView.postMessage(message)
+      }
+    });
+  }
+
+  closeDevice (deviceIdentifier) {
+    const messageId = this._messageId++
+
+    const message = {
+      type: 'closeDevice',
+      messageId,
+      data: {
+        device: deviceIdentifier
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      // If the native app hasn't responded in 3 secs then timeout
+      const rpcTimeout = setTimeout(() => {
+        window.removeEventListener('message', returnHandler);
+        reject(new Error(`Timeout calling function: ${message.type}`));
+      }, 3 * 1000);
+
+      const returnHandler = (event) => {
+        const {data: message} = event.data;
+
+        if (this._rpcSuccessful(message, messageId, 'deviceClosed')) {
+          resolve(message.response);
+          clearTimeout(rpcTimeout);
+          window.removeEventListener('message', returnHandler);
+        }
+
+        reject(new Error(message.error));
+        clearTimeout(rpcTimeout);
+        window.removeEventListener('message', returnHandler);
+      };
+
+      window.addEventListener('message', returnHandler);
+
+      if ('flutter_inappwebview' in window) {
+        return window.flutter_inappwebview.callHandler(message.type, message);
+      }
+
+      if ('ReactNativeWebView' in window) {
+        return window.ReactNativeWebView.postMessage(message)
+      }
+    });
+  }
+}
+
+export default new NativeRpc();
