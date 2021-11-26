09a9a4ce54d1e88f1d9d8dbe8a5842a2858659de
Eliot Howes <eliot@medicspot.co.uk>
2021-11-26 14:33:19 +0000

    improve data presentation

--- src/Pages/NativePlayground.js
+++ src/Pages/NativePlayground.js
@@ -186,8 +186,8 @@ const NativePlayground = () => {
             <div key={JSON.stringify(reading)}>
               <p>SpO2: {reading.spO2}</p>
               <p>Pulse: {reading.pulse}</p>
-              <p>Finger in: {reading.fingerIn}</p>
-              <p>Status: {String(reading.searching)}</p>
+              <p>Finger in: {String(reading.fingerIn)}</p>
+              <p>Searching: {String(reading.searching)}</p>
             </div>
           )
         })}
